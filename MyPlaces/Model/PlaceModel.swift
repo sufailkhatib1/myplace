6934a409b62f0d9a4e40b0749e3932b7604228fa
sufail khatib <sufailkhatib@icloud.com>
2020-10-22 13:02:45 +0200

    uploading my project

---
+++ MyPlaces/Model/PlaceModel.swift
@@ -0,0 +1,22 @@
+//
+//  PlaceModel.swift
+//
+//
+
+import Foundation
+import UIKit
+
+class PlaceModel {
+    
+    static let sharedInstance = PlaceModel()
+    
+    var placeName = ""
+    var placeType = ""
+    var placeAtmosphere = ""
+    var placeImage = UIImage()
+    var placeLatitude = ""
+    var placeLongitude = ""
+    
+    private init(){}
+    
+}
